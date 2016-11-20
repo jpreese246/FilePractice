3007cf0803a31ffb1e31fe1bf3d4559c764a7b83
jpreese246 <jpreese@gmail.com>
2016-11-19 20:02:47 -0500

    Initial Commit

---
+++ Expenselt/Expenselt/ExpensltHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expenselt
+{
+    /// <summary>
+    /// Interaction logic for ExpensltHome.xaml
+    /// </summary>
+    public partial class ExpensltHome : Page
+    {
+        public ExpensltHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
